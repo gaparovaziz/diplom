53705ff1ef85e589e1d3a8347055f8514e7e5519
Aziz Gaparov <azizgithubacc@gmail.com>
2022-06-28 15:13:19 +0600

    show error message over form

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -26,14 +26,11 @@ export default function Auth() {
   navigate('/');
 }
 
-if (error) {
-  alert(error)
-}
-
 
  return (
   <form onSubmit={onAuthStart}>
    <div className="text-center modal modal-signin position-static d-block">
+    <h4 style={{color: 'red'}}>{error}</h4>
     <main className="form-signin w-100 m-auto" style={{ maxWidth: "350px" }}>
      <div className="formSignIn bg-white rounded p-3 bg-secondary shadow-lg">
       <img src='https://d1yjjnpx0p53s8.cloudfront.net/logo-fitform_2.jpg' alt="" width="76" height="76" className="rounded"/>
